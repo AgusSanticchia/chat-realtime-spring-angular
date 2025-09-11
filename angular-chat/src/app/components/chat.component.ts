7e4624a9e397c4f167c9e2a505e3b0cb70e41350
Agustin Santicchia <santicchiaagus85@gmail.com>
2025-09-11 14:00:28 -0300

    feat: add chat HTML template with send/receive message functionality

--- angular-chat/src/app/components/chat.component.ts
+++ angular-chat/src/app/components/chat.component.ts
@@ -1,23 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms'
-import * as Stomp from '@stomp/stompjs'
+import { FormsModule } from '@angular/forms';
+import * as Stomp from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
+import { Message } from '../models/message';
 
 @Component({
   selector: 'app-chat',
-  standalone: true,
   imports: [FormsModule, CommonModule],
-  templateUrl: './chat.component.html',
+  templateUrl: './chat.component.html'
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit{
   client!: Stomp.Client;
   connected: boolean = false;
+
+  messages: Message[] = [];
+  message: Message = new Message();
 
   ngOnInit(): void {
     this.client = new Stomp.Client({
       brokerURL: undefined,
-      webSocketFactory: () => new SockJS('http://localhost:8080'),
+      webSocketFactory: () => new SockJS('http://localhost:8080/chat-websocket'),
       debug: str => console.log(str),
       reconnectDelay: 5000
     });
@@ -25,22 +28,37 @@ export class ChatComponent implements OnInit {
     this.client.onConnect = (frame) => {
       this.connected = true;
       console.log(`Connected: ${this.client.connected} : ${frame}`)
+
+      this.client.subscribe('/chat/message', e => {
+        console.log(e.body)
+        let message: Message = JSON.parse(e.body) as Message;
+        message.date = new Date(message.date)
+        this.messages.push(message);
+      })
     }
 
-    this.client.onDisconnect = (frame) =>{
+    this.client.onDisconnect = (frame) => {
       this.connected = false;
-      console.log(`Disconnected ${this.client.connected} : ${frame}`)
+      this.message = new Message();
+      this.messages = [];
+      console.log(`Disconnected: ${!this.client.connected} : ${frame}`)
     }
-    this.connect();
   }
 
-  connect(): void{
+  connect(): void {
     this.client.activate();
   }
 
-  deconnect(): void{
+  deconnect(): void {
     this.client.deactivate();
   }
 
+  onSendMessage() {
+    this.client.publish({
+      destination: '/app/message',
+      body: JSON.stringify(this.message)
+    });
+    this.message.text = '';
+  }
+
 }
- 
